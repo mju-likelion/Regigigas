fd9b5a37b8a2d10a2f3f9c21b5a79a4c8437eb97
minjung <ypd06021@naver.com>
2022-04-17 14:17:14 +0900

    fix: nodemon 오류 처리 및 web-api-platform-error 해결

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { App } from "@slack/bolt";
 import dotenv from "dotenv";
-import express from "express";
 
-import anonymousBot from "./ anonymousBot";
-// import addBirthdayEvents from "./slack/addBirthdayEvents";
+// import anonymousBot from "./ anonymousBot";
 
-express();
 dotenv.config();
 const port = Number(process.env.PORT) || 3000;
 
@@ -17,46 +14,14 @@ const app = new App({
 });
 
 // anonymousBot(app);
-// addBirthdayEvents(app);
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
+app.message('hi parrot', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
+  // @ts-expect-error
   await say(`Hey there <@${message.user}>!`);
-});
-
-// Listens to incoming messages that contain "hello"
-app.message('qwer', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click" // This will act as a unique identifier for the button so your app can specify what action it wants to respond to.
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
   await app.start(port);
-  console.log("⚡️ Bolt app is running!");
+  console.log("⚡️ Bolt app is running!  ");
 })();
--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,56 @@
-import { WebClient } from '@slack/web-api';
+import { App } from "@slack/bolt";
+import dotenv from "dotenv";
+// import express from "express";
 
-// An access token (from your Slack app or custom integration - xoxp, xoxb)
-// const token = Number(process.env.SLACK_BOT_TOKEN);
+import anonymousBot from "./ anonymousBot";
+// import addBirthdayEvents from "./slack/addBirthdayEvents";
 
-const web = new WebClient(process.env.SLACK_BOT_TOKEN);
+// express();
+dotenv.config();
+const port = Number(process.env.PORT) || 3000;
 
-// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = 'C1232456';
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, // add this
+  appToken: process.env.SLACK_APP_TOKEN, // add this
+});
+
+anonymousBot(app);
+// addBirthdayEvents(app);
+
+// // Listens to incoming messages that contain "hello"
+// app.message('qwer', async ({ message, say }) => {
+//   // say() sends a message to the channel where the event was triggered
+//   await say({
+//     blocks: [
+//       {
+//         "type": "section",
+//         "text": {
+//           "type": "mrkdwn",
+//           "text": `Hey there <@${message.user}>!`,
+//         },
+//         "accessory": {
+//           "type": "button",
+//           "text": {
+//             "type": "plain_text",
+//             "text": "Click Me",
+//           },
+//           "action_id": "button_click", // This will act as a unique identifier for the button so your app can specify what action it wants to respond to.
+//         },
+//       },
+//     ],
+//     text: `Hey there <@${message.user}>!`,
+//   });
+// });
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
 
 (async () => {
-  // See: https://api.slack.com/methods/chat.postMessage
-  const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there' });
-
-  // `res` contains information about the posted message
-  // console.log('Message sent: ', res.ts);
+  await app.start(port);
+  console.log("⚡️ Bolt app is running! ");
 })();
