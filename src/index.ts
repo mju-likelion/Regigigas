99b1498c34c15a372999f32208b021f0104ff873
minjung <ypd06021@naver.com>
2022-04-24 03:36:30 +0900

    feat: 모달버전 추가

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 // 여기에 각자 파일 import 해와주세요.(알파벳 순으로)
 import anonymous from "./ anonymous";
 import introduce from "./introduce";
-import slash from "./lib/slash"
 
 dotenv.config();
 const port = Number(process.env.PORT) || 3000;
@@ -19,7 +18,6 @@ const app = new App({
 // 여기 아래와 같은 형식으로 본인꺼 추가해주세요.
 introduce(app);
 anonymous(app);
-slash(app);
 
 (async () => {
   await app.start(port);
--- src/lib/modals.ts
+++ src/lib/modals.ts
@@ -1,115 +1,18 @@
 import { App } from "@slack/bolt";
 
     function modals(app: App) {
+      app.message('모달', async ({ message, say }) => {
+        // @ts-expect-error
+        await say(`<@${message.user.id}>  `);
+      });
 
-        app.action('morning_button', async ({ ack }) => {});
-
-        slackApp.action("morning_button", async ({ body, ack, say, client }) => {
-            await ack();
-            try {
-              const result = await client.views.open({
-                trigger_id: body.trigger_id,
-                view: {
-                  type: "modal",
-                  callback_id: "morning_modal",
-                  title: {
-                    type: "plain_text",
-                    text: "미라클 모닝 챌린지",
-                  },
-                  blocks: [
-                    {
-                      type: "section",
-                      text: {
-                        type: "mrkdwn",
-                        text: "오늘의 문제.",
-                      },
-                    },
-                    {
-                      type: "input",
-                      block_id: "question",
-                      label: {
-                        type: "plain_text",
-                        text: "1 + 1 = ?",
-                      },
-                      element: {
-                        type: "plain_text_input",
-                        action_id: "answer",
-                      },
-                    },
-                  ],
-                  submit: {
-                    type: "plain_text",
-                    text: "Submit",
-                  },
-                },
-              });
-              console.log(result);
-            } catch (error) {
-              console.error(error);
-            }
-        });
-
-        // Listen for a slash command invocation
-        app.command('/modals', async ({ ack, body, client, logger }) => {
-            // Acknowledge the command request
-            await ack();
-
-            try {
-                // Call views.open with the built-in client
-                const result = await client.views.open({
-                    // Pass a valid trigger_id within 3 seconds of receiving it
-                    trigger_id: body.trigger_id,
-                    // View payload
-                    view: {
-                        type: 'modal',
-                        // View identifier
-                        callback_id: 'view_1',
-                        title: {
-                            type: 'plain_text',
-                            text: 'Modal title',
-                        },
-                        blocks: [
-                            {
-                            type: 'section',
-                            text: {
-                                type: 'mrkdwn',
-                                text: 'Welcome to a modal with _blocks_'
-                            },
-                            accessory: {
-                                type: 'button',
-                                text: {
-                                type: 'plain_text',
-                                text: 'Click me!',
-                                },
-                                action_id: 'button_abc',
-                            }
-                            },
-                            {
-                            type: 'input',
-                            block_id: 'input_c',
-                            label: {
-                                type: 'plain_text',
-                                text: 'What are your hopes and dreams?'
-                            },
-                            element: {
-                                type: 'plain_text_input',
-                                action_id: 'dreamy_input',
-                                multiline: true
-                            },
-                            },
-                        ],
-                        submit: {
-                            type: 'plain_text',
-                            text: 'Submit',
-                        },
-                    },
-                });
-                logger.info(result);
-            }
-            catch (error) {
-                logger.error(error);
-            }
-        });
+      // anonymous.ts 수정
+      // app.command('/모달버전', async ({ command , say }) => {
+      //     // 모달 액션 넣기
+      //     modals(app);
+      //     // 테스트
+      //     await say(`텍스트는 : ${command.text}`);
+      // });
     }
         
 
