d4fc290be8361d355460185c3016546c5529e975
Bhanjo <hanjo1515@naver.com>
2022-04-27 23:49:22 +0900

    chor: 주사위 이벤트 명령어 수정 및 이벤트 목록과 리액션 함수를 분리

--- src/randomNumber.ts
+++ src/randomNumber.ts
@@ -12,60 +12,55 @@ const reactionRange = (dice: number): string => {
   return "reactionRange error: 1~100 까지의 수만 표현합니다";
 };
 
+const reactionEvents = [
+  {
+    id: "bad",
+    text: [
+      "어이쿠 손이 미끄러졌네 :coskmos:",
+      "제 잘못 아니에요 :musuktard:",
+      "제가 해도 이것보단 잘 나오겠네요 :zzzzzz:",
+    ],
+  },
+  {
+    id: "notbad",
+    text: ["뭐..네 :nodnod:", "아깝네요 :crying:", "재도전 합시다 우리 :eyes:"],
+  },
+  {
+    id: "normal",
+    text: [
+      "이정도면 잘 나온거죠 :smile:",
+      "재도전 하시겠어요? :thinking__face:",
+      "평범하네요 :meow_code:",
+    ],
+  },
+  {
+    id: "great",
+    text: [
+      "오늘 좀 치는 날인가요? :eeee:",
+      "꽤나 높은 점수에요! :fb_wow:",
+      "제 덕분인거 알죠? :sunglasses:",
+    ],
+  },
+  {
+    id: "excellent",
+    text: [
+      "오늘 로또 각이에요 :daebak:",
+      "미쳤다 :crazy:",
+      "잘했죠? 빨리 칭찬해주세요 :meow_dundundun:",
+    ],
+  },
+];
+
 const reaction = (dice: number): string => {
   const actionType = reactionRange(dice);
   const actionEvent = randomValue(0, 3);
-
-  const reactionEvents = [
-    {
-      id: "bad",
-      text: [
-        "어이쿠 손이 미끄러졌네 :coskmos:",
-        "제 잘못 아니에요 :musuktard:",
-        "제가 해도 이것보단 잘 나오겠네요 :zzzzzz:",
-      ],
-    },
-    {
-      id: "notbad",
-      text: [
-        "뭐..네 :nodnod:",
-        "아깝네요 :crying:",
-        "재도전 합시다 우리 :eyes:",
-      ],
-    },
-    {
-      id: "normal",
-      text: [
-        "이정도면 잘 나온거죠 :smile:",
-        "재도전 하시겠어요? :thinking__face:",
-        "평범하네요 :meow_code:",
-      ],
-    },
-    {
-      id: "great",
-      text: [
-        "오늘 좀 치는 날인가요? :eeee:",
-        "꽤나 높은 점수에요! :fb_wow:",
-        "제 덕분인거 알죠? :sunglasses:",
-      ],
-    },
-    {
-      id: "excellent",
-      text: [
-        "로또 각이에요 :daebak:",
-        "미쳤다 :crazy:",
-        "잘했죠? 빨리 칭찬해주세요 :meow_dundundun:",
-      ],
-    },
-  ];
   const activeEvent = reactionEvents.find((item) => item.id === actionType);
   return activeEvent?.text[actionEvent] || "reaction error";
 };
 
 const randomNunber = (app: App) => {
-  app.message("자비스 굴려", async ({ message, say }) => {
+  app.message("자비스 던져", async ({ message, say }) => {
     const diceValues = randomValue();
-
     let answer: string = "";
 
     if (diceValues === 0) {
