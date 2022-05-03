e0ac87b0a0419c0f717f43c2f0ed0cbce56200f1
parkmincheol <maxandy@naver.com>
2022-05-04 00:49:56 +0900

    feat: 점심 메뉴 추천

--- src/lunchMenu.ts
+++ src/lunchMenu.ts
@@ -1,4 +1,6 @@
+/* eslint-disable import/extensions */
 import { App } from '@slack/bolt';
+
 import gangnam from './lunchList/gangnam.json';
 import yongin from './lunchList/yongin.json';
 
@@ -6,12 +8,16 @@ const lunchMenu = (app: App) => {
   app.message('강남 뭐 먹지', async ({ say }) => {
     const pickedGangnam = Math.floor(Math.random() * gangnam.length);
     const gangnamRestaurant = gangnam[pickedGangnam] || [];
-    await say(`오늘 점심은 ${gangnamRestaurant.gangnamName} 어때요? /n ${gangnamRestaurant.gangnamUrl}`);
+    await say(
+      `오늘 점심은 ${gangnamRestaurant.gangnamName} 어때요? /n ${gangnamRestaurant.gangnamUrl}`,
+    );
   });
   app.message('용인 뭐 먹지', async ({ say }) => {
     const pickedYongin = Math.floor(Math.random() * yongin.length);
     const yonginRestaurant = yongin[pickedYongin] || [];
-    await say(`오늘 점심은 ${yonginRestaurant.yonginName} 어때요? /n ${yonginRestaurant.yonginUrl}`);
+    await say(
+      `오늘 점심은 ${yonginRestaurant.yonginName} 어때요? /n ${yonginRestaurant.yonginUrl}`,
+    );
   });
 };
 
