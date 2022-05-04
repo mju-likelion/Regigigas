dbbecf00246ed1270d30d98e59c3a3ebda5618aa
parkmincheol <maxandy@naver.com>
2022-05-04 13:01:39 +0900

    feat: 점심 메뉴 추천

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { App } from "@slack/bolt";
-import dotenv from "dotenv";
+import { App } from '@slack/bolt';
+import dotenv from 'dotenv';
 
 // 여기에 각자 파일 import 해와주세요.(알파벳 순으로)
-import anonymous from "./anonymous";
-import introduce from "./introduce";
-import randomNumber from "./randomNumber";
+import anonymous from './anonymous';
+import introduce from './introduce';
+import lunchMenu from './lunchMenu';
+import randomNumber from './randomNumber';
 
 dotenv.config();
 const port = Number(process.env.PORT) || 3000;
@@ -19,6 +20,7 @@ const app = new App({
 // 여기 아래와 같은 형식으로 본인꺼 추가해주세요.
 anonymous(app);
 introduce(app);
+lunchMenu(app);
 randomNumber(app);
 
 (async () => {
---
+++ src/lunchMenu.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/extensions */
+import { App } from '@slack/bolt';
+
+import gangnam from './lunchList/gangnam.json';
+import yongin from './lunchList/yongin.json';
+
+const lunchMenu = (app: App) => {
+  app.message('강남 뭐 먹지', async ({ say }) => {
+    const pickedGangnam = Math.floor(Math.random() * gangnam.length);
+    const gangnamRestaurant = gangnam[pickedGangnam] || [];
+    await say(
+      `오늘 점심은 ${gangnamRestaurant.gangnamName} 어때요? /n ${gangnamRestaurant.gangnamUrl}`,
+    );
+  });
+  app.message('용인 뭐 먹지', async ({ say }) => {
+    const pickedYongin = Math.floor(Math.random() * yongin.length);
+    const yonginRestaurant = yongin[pickedYongin] || [];
+    await say(
+      `오늘 점심은 ${yonginRestaurant.yonginName} 어때요? /n ${yonginRestaurant.yonginUrl}`,
+    );
+  });
+};
+
+export default lunchMenu;
