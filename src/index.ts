06c3fea712f3e8199f04085919698694047a0b03
qkrthdus605 <qkrthdus605@gmail.com>
2022-05-11 03:37:54 +0900

    feat: 생일 축하 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import dotenv from 'dotenv';
 
 // 여기에 각자 파일 import 해와주세요.(알파벳 순으로)
 import anonymous from './anonymous';
+import birthday from './birthday';
 import introduce from './introduce';
-import randomNumber from './randomNumber';
 import jarvisCalling from './jarvisCalling';
+import randomNumber from './randomNumber';
 
 dotenv.config();
 const port = 3000;
@@ -19,6 +20,7 @@ const app = new App({
 
 // 여기 아래와 같은 형식으로 본인꺼 추가해주세요.
 anonymous(app);
+birthday(app);
 introduce(app);
 randomNumber(app);
 jarvisCalling(app);
