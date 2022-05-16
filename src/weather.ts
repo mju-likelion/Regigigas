eae4ad6b80f1f3265590889ef75c3a25da9011b3
minjung <ypd06021@naver.com>
2022-05-16 21:14:08 +0900

    feat: fetch로 응답받기

--- src/weather.ts
+++ src/weather.ts
@@ -1,15 +1,21 @@
 import { App } from '@slack/bolt';
+import fetch from 'cross-fetch';
+
+const today = new Date();
+const hours: number = today.getHours();
+const url =
+  'https://api.openweathermap.org/data/2.5/weather?lat=37.225004&lon=127.187690&appid=3a3faad2ff8d32bd5b413c9dbdc82048';
 
 function weather(app: App) {
-  app.message('자비스 날씨', async ({ message, say }) => {
-    // say() se–nds a message to the channel where the event was triggered
+  // 현재 학교 날씨
+  app.message('날씨', async ({ message, say }) => {
     await say({
       blocks: [
         {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `그 정도는 직접 찾아보세요`,
+            text: `창문 밖을 보면 알 수 있잖아요;;`,
           },
           accessory: {
             type: 'button',
@@ -17,7 +23,7 @@ function weather(app: App) {
               type: 'plain_text',
               text: '현재 학교날씨',
             },
-            action_id: 'button_click',
+            action_id: 'button_click_1',
           },
         },
       ],
@@ -27,10 +33,14 @@ function weather(app: App) {
   });
 
   // 버튼 클릭
-  app.action('button_click', async ({ ack, say }) => {
-    // Acknowledge the action
+  app.action('button_click_1', async ({ ack, say }) => {
     await ack();
-    await say(`학교날씨`);
+    // 날씨 api 찌르기 - fetch
+    fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data.weather));
+    // 응답
+    await say(`학교날씨는 ${today}`);
   });
 }
 
