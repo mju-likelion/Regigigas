cbc746509b5d6413ce117297beaa32abf2bc5162
minjung <ypd06021@naver.com>
2022-05-16 22:28:50 +0900

    feat: 날씨 별 응답 추가

--- src/weather.ts
+++ src/weather.ts
@@ -1,8 +1,6 @@
 import { App } from '@slack/bolt';
 import fetch from 'cross-fetch';
 
-const today = new Date();
-const hours: number = today.getHours();
 const url =
   'https://api.openweathermap.org/data/2.5/weather?lat=37.225004&lon=127.187690&appid=3a3faad2ff8d32bd5b413c9dbdc82048';
 
@@ -15,7 +13,7 @@ function weather(app: App) {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `창문 밖을 보면 알 수 있잖아요;;`,
+            text: `이 정도는 직접 찾아보세요;;`,
           },
           accessory: {
             type: 'button',
@@ -38,9 +36,30 @@ function weather(app: App) {
     // 날씨 api 찌르기 - fetch
     fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data.weather));
-    // 응답
-    await say(`학교날씨는 ${today}`);
+      .then(data => {
+        const youngin = data.weather;
+        const object = youngin[0].main;
+        // 맑을 때
+        if (object === 'Clear') {
+          say(`:sunny:`);
+        }
+        // 구름
+        else if (object === 'Cloud') {
+          say(`:cloud:`);
+        }
+        // 눈
+        else if (object === 'Snow') {
+          say(`:snowflack:`);
+        }
+        // 비
+        else if (object === 'Rain') {
+          say(`:umbrella_with_rain_drops:`);
+        }
+        // 그 밖
+        else {
+          say(`저도 모르겠어요...:loading:`);
+        }
+      });
   });
 }
 
