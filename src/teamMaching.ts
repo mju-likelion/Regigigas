4f41667d827c4824d9dc48c1a8a10e63f6168e67
Bhanjo <hanjo1515@naver.com>
2022-05-18 18:03:39 +0900

    docs: 주석 추가

--- src/teamMaching.ts
+++ src/teamMaching.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-plusplus */
-/* eslint-disable no-console */
 import { App } from '@slack/bolt';
 
 const randomTeamIndex = (from: number = 0, to: number = 5): number =>
@@ -10,13 +9,14 @@ const randomPartition = (teams: number, players: string) => {
   let maxTeamSize = Math.floor(players.length / teams);
   let breakPoint = 0;
 
+  // 팀에 사람을 넣기 위한 2차원 배열 선언 (ES6)
   // eslint-disable-next-line no-array-constructor
   const team = Array.from(Array(teams), () => new Array());
 
   // 균등하게 팀 배분하기
   while (playerIdx < teams * maxTeamSize) {
     const teamIdx = randomTeamIndex(0, teams);
-    // 해당 팀에 사람들 넣기
+    // 팀에 넣을 수 있다면 사람 넣기
     if (team[teamIdx].length < maxTeamSize) {
       team[teamIdx].push(players[playerIdx]);
       playerIdx += 1;
@@ -27,7 +27,7 @@ const randomPartition = (teams: number, players: string) => {
     breakPoint += 1;
   }
 
-  // 남는 사람 배치
+  // 남는 사람 배치, 남는 사람들을 넣기 위해 maxTeamSize + 1
   maxTeamSize += 1;
   while (playerIdx < players.length) {
     const teamIdx = randomTeamIndex(0, teams);
@@ -47,17 +47,19 @@ const teamMaching = (app: App) => {
     // @ts-expect-error
     const { text } = message;
 
-    // 팀과 플레이어를 구분
+    // 명령어와 팀, 플레이어를 구분
     const teamData = text.split(' ');
     const numberOfTeams = Number(teamData[2]);
     const numberOfPlayers = teamData.splice(3);
 
     if (numberOfTeams <= numberOfPlayers.length && numberOfPlayers.length <= 100) {
+      // 조건에 맞다면 팀 나누기 기능 실행
       const dividedTeams = randomPartition(numberOfTeams, numberOfPlayers);
+      // 팀 배열을 원하는 문자 스타일로 매핑
       const teamList = dividedTeams.map((item, idx) => `${idx + 1}조: ${item}\n`).join('');
       await say(`제가 열심히 짜봤어요 :blush:\n${teamList} 어때요? 마음에 드시나요? ༼ つ ◕_◕ ༽つ`);
     } else {
-      const errorMessage = `[팀 매칭 에러!] 다음을 확인해주세요. 팀 수 <= 팀 인원 / 팀 인원 <= 100 / '자비스 팀매칭 팀개수 사람1 사람2 사람3'`;
+      const errorMessage = `[팀 매칭 에러!] 다음을 확인해주세요. 팀 수 <= 팀 인원 / 팀 인원 <= 100 / '자비스 팀매칭 (팀 수) 사람1 사람2 사람3'`;
       await say(`${errorMessage}`);
     }
   });
